2349d4bab9eafbaafe6ab32a8f5f13f5d69eb73c
Laura Carvajal <43411408+LauraCarvajalAguilar@users.noreply.github.com>
2019-06-13 18:20:33 -0500

    Add files via upload

---
+++ MetodosU6/src/metodos/Entrada.java
@@ -0,0 +1,14 @@
+package metodos;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+         
+    Inicio ini = new Inicio();
+    ini.setVisible(true);
+    ini.setLocationRelativeTo(null);
+    
+     }
+    }
+    
+
---
+++ MetodosU6/src/metodos/Metodos_Numericos_TI.java
@@ -0,0 +1,11 @@
+package metodos;
+
+public class Metodos_Numericos_TI {
+
+    public static void main(String[] args) {
+    
+    Inicio ini=new Inicio();
+    ini.setVisible(true);
+    ini.setLocationRelativeTo(null);
+   }
+}
